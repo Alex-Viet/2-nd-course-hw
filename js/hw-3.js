468c015e20ac3a98f18b9d38a884b8b8db699306
Alex-Viet <alex.night.power@gmail.com>
2023-03-30 02:03:42 +0300

    Homework_2-3 Task_1-5 is ready

---
+++ js/hw-3.js
@@ -0,0 +1,54 @@
+/*let password = 'krab123';
+let userPass = prompt('Введите пароль');
+if (userPass === password) {
+    console.log('Пароль введен верно');
+} else {
+    console.log('Пароль введен неправильно');  // Задание 1
+}*/
+
+/*let c = 2;
+if (c > 0 && c < 10) {
+    console.log('Верно')
+} else {
+    console.log('Неверно');  // Задание 2
+}*/
+
+/*let d = -2;
+let e = 101;
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');  // Задание 3
+}*/
+
+/*let a = Number('2');
+let b = Number('3');  // Задание 4
+alert(a + b);*/
+
+/*let monthNumber = Number(prompt('Введите номер месяца, чтобы узнать курс рубля'));
+switch (monthNumber) {
+    case 12:
+    case 1:
+    case 2:
+        console.log('Зима');
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Весна');
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log('Лето');
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log('Осень');
+        break;
+    default:
+        console.log('Нет такого месяца');   // Задание 5
+        break;
+}*/
+
