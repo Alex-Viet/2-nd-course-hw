fd546debff41c199b8f8c2ec2cdb0939ecdf1dce
Alex-Viet <alex.night.power@gmail.com>
2023-04-05 17:39:53 +0300

    Homework2-4 is ready for check

---
+++ js/hw-4.js
@@ -0,0 +1,41 @@
+/*let h = 1;
+while (h < 3) {
+    console.log('Привет');  // Задание 1
+    h++;
+}*/
+
+/*let a = 1;
+while (a < 6) {
+    console.log(a);  // Задание 2
+    a++;
+}*/
+
+/*let numb = 7;
+while (numb >= 7 && numb <= 22) {  // Задание 3
+        console.log(numb);
+        numb++;
+}*/
+
+/*const obj = {
+    "Коля": 200,
+    "Вася": 300,
+    "Петя": 400,
+}
+for (const name in obj) {
+    if (obj[name]) {
+        console.log(`${name} - зарплата ${obj[name]} долларов`);  // Задание 4
+    }
+}*/
+
+/*let n = 1000;
+let num = 0;
+while (n > 50) {
+    console.log(n);
+    n /= 2;
+    num++;
+}
+console.log(num);*/   // Задание 5
+
+/*for (let day = 1; day <= 31; day += 7) {
+    console.log(`Сегодня пятница, ${day} число. Необходимо подготовить отчет.`); // Задание 6 - как указать, что пятница может быть либо 1-м числом, либо 7?
+}*/
