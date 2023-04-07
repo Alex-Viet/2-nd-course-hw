305e19b82ccf6cf48d1178a1eb8a7caf05b4f928
Alex-Viet <alex.night.power@gmail.com>
2023-04-07 18:37:23 +0300

    Homework 2-5, tasks 1-5 is ready (JS)

---
+++ js/hw-5.js
@@ -0,0 +1,63 @@
+/*function getMinorityNumb(a, b) {
+    if (a <= b) {
+        return(a);
+    } else {
+        return(b);
+    }
+}
+
+console.log(getMinorityNumb(1, 2));*/    // Задание 1
+
+
+/*let getEven = (x) => {
+    if (x % 2 == 0) {
+        return console.log('Число четное');
+    } else {
+        return console.log('Число нечетное');
+    }
+}
+
+getEven(Number(0));*/    // Задание 2
+
+
+/*function calcSqrt(n) {
+    console.log(n**2);
+}
+
+calcSqrt(10);      // Задание 3.1
+
+
+function calcSqrt(n) {
+    return n**2;
+}
+
+console.log(calcSqrt(13));*/   // Задание 3.2
+
+
+/*let findAge = () => {
+    const userAge = Number(prompt('Сколько вам лет?'));
+    if (userAge < 0) {
+        console.log('Вы ввели неправильное значение');
+    } else if (userAge >= 0 && userAge <=12) {
+        console.log('Привет, друг!');
+    } else {
+        console.log('Добро пожаловать!');
+    }
+}
+
+findAge();*/    // Задание 4
+
+
+/*function isNumb(a, b) {
+    return !isNaN(a) && !isNaN(b) ? a * b : console.log('Одно или оба значения не являются числом');
+
+    // if ( a !== isNaN(a) && b !== isNaN(b)) {
+    //     return a * b;
+    // } else {
+    //     return console.log('Одно или оба значения не являются числом');
+    // }
+
+}
+
+console.log(isNumb(5, 11));*/     // Задание 5
+
