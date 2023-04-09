4697f27115ceab377647d1acf5ccaf58fac2e826
Alex-Viet <alex.night.power@gmail.com>
2023-04-09 21:43:50 +0300

    tasks 4-6 is ready

--- js/hw-6.js
+++ js/hw-6.js
@@ -7,10 +7,33 @@ for (let i = 0; i < numbers.length; i++) {
 
 
 /*const numb = [1, 5, 4, 10, 0, 3];
-console.log(numb.indexOf(4));*/   // Задание 2
+console.log(numb.indexOf(4));*/    // Задание 2
 
 
 /*const numbrs = [1, 3, 5, 10, 20];
-console.log(numbrs.join(' '));*/   // Задание 3
+console.log(numbrs.join(' '));*/    // Задание 3
 
 
+/*const arr = [];
+
+for (let subArr = 0; subArr < 3; subArr++) {
+    arr[subArr] = [];
+    
+    for (let numb = 0; numb < 3; numb++) {
+        arr[subArr].push(1);
+    }
+}
+
+console.log(arr);*/         // Задание 4
+
+
+/*const numbs = [1, 1, 1];
+numbs.push(2, 2, 2);
+console.log(numbs);*/    // Задание 5
+
+
+/*const arr = [9, 8, 7, 'a', 6, 5];
+arr.sort();
+arr.pop();
+
+console.log(arr);*/     // Задание 6
