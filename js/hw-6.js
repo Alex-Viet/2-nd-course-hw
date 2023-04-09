89553516e22a5fec046a218f19216ae4da0cb74f
Alex-Viet <alex.night.power@gmail.com>
2023-04-09 09:57:53 +0300

    Added hw-6.js and tasks 1-3 is ready

---
+++ js/hw-6.js
@@ -0,0 +1,16 @@
+/*const numbers = [1, 5, 4, 10, 0, 3];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 0) break;
+    console.log(numbers[i]);      // Задание 1
+}*/
+
+
+/*const numb = [1, 5, 4, 10, 0, 3];
+console.log(numb.indexOf(4));*/   // Задание 2
+
+
+/*const numbrs = [1, 3, 5, 10, 20];
+console.log(numbrs.join(' '));*/   // Задание 3
+
+
