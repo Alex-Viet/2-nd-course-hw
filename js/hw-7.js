8163b5ff3b1aeeaf736c1216cc5992d19eb1664b
Alex-Viet <alex.night.power@gmail.com>
2023-04-13 03:30:05 +0300

    Added hw-7.js, completed tasks 1-2

---
+++ js/hw-7.js
@@ -0,0 +1,16 @@
+/*let str = 'js';
+str = str.toUpperCase();
+console.log(str);*/   // Задание 1
+
+
+/*const arrayStr = ['которая', 'в', 'качестве', 'параметров', 'принимает', 'массив', 'строк'];
+let searchWord = 'пАР';
+
+function searchBeginString(array, search) {
+    const resultArrStr = array.filter((el) => el.toLowerCase().startsWith(search.toLowerCase()));
+    return resultArrStr;
+}
+
+console.log(searchBeginString(arrayStr, searchWord));*/   // Задание 2
+
+
