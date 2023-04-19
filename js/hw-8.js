6a2165672ae5435270cd7a9fa35ecb1a89e27e42
Alex-Viet <alex.night.power@gmail.com>
2023-04-19 13:38:51 +0300

    Tasks 3-5 completed. Homework2-8 is ready

--- js/hw-8.js
+++ js/hw-8.js
@@ -3,11 +3,11 @@
 }
 
 function getSum(array) {
-    return array.reduce((n, m) => (n + m));
+    return Math.round(array.reduce((n, m) => (n + m)));
 }
 
 function getMult(array) {
-    return array.reduce((n, m) => (n * m));
+    return Math.round(array.reduce((n, m) => (n * m)));
 }
 
 getResult([4, 5, 3, 10], getSum);
@@ -30,6 +30,55 @@ function getSortedArrObj(array) {
     console.log(array.sort(compareFunction));
 }
 
-getSortedArrObj(users);*/       // Задание 2
+getSortedArrObj(users);*/      // Задание 2
 
 
+/*const each = (arr, callback) => {
+    console.log(callback(arr));
+}
+
+function reversArr(array) {
+    return array.reverse();
+}
+
+function toNumberArr(array) {
+    return array.map((item) => Number(item)).filter((el) => !isNaN(el));
+}
+
+each([1, '4', 9, 'two'], reversArr);
+each([1, '4', false, 9, 'two'], toNumberArr);*/    // Задание 3
+
+
+/*const getTimer = (deadline) => {
+	let time = deadline;
+	const interval = setInterval(() => {
+		time -= 3;
+		console.log(new Date);
+	}, 3000);
+
+	setTimeout(() => {
+    clearInterval(interval);
+    console.log('30 секунд прошло');
+  }, deadline * 3000)
+}
+
+getTimer(10);*/      // Задание 4
+
+
+/*function calling() {
+    console.log('Звоню!')
+};
+
+function beeps(callback) {
+    setTimeout(() => {
+        console.log('Идут гудки...');
+        callback();
+    }, 1000);
+}
+
+function talk() {
+    console.log('Разговор')
+}
+
+calling();
+beeps(talk);*/     // Задание 5
