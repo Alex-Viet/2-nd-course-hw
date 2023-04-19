731d44eafecb4416516e284e266f05c8dfe5f5d3
Alex-Viet <alex.night.power@gmail.com>
2023-04-19 10:00:31 +0300

    New file hw-8.js. Tasks 1-2 completed

---
+++ js/hw-8.js
@@ -0,0 +1,35 @@
+/*function getResult(numbs, callback) {
+    console.log(callback(numbs));
+}
+
+function getSum(array) {
+    return array.reduce((n, m) => (n + m));
+}
+
+function getMult(array) {
+    return array.reduce((n, m) => (n * m));
+}
+
+getResult([4, 5, 3, 10], getSum);
+getResult([4, 5, 3, 10], getMult);*/     // Задание 1
+
+
+/*const users = [
+	{name: 'Jon', age: 22},
+	{name: 'Richard', age: 18},
+	{name: 'Anton', age: 32},
+	{name: 'Lida', age: 23},
+	{name: 'Bob', age: 44}
+];
+
+function compareFunction(a, b) {
+	return a.age - b.age;
+}
+
+function getSortedArrObj(array) {
+    console.log(array.sort(compareFunction));
+}
+
+getSortedArrObj(users);*/       // Задание 2
+
+
